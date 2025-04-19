4e4f1034b849177d35cdb8c772e79a04cbebd7be
Aidan <aidan.heathfield@gmail.com>
2025-04-19 00:47:05 -0400

    changed some of the theme colours for catppuccin

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -5,6 +5,64 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {
+          mocha = {
+            overlay2 = '#5d5469', -- things like comments
+            mauve = '#f996fc', -- things like function() or return
+            lavender = '#907ef3', -- things like lavender
+          },
+        },
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          telescope = {
+            enabled = true,
+            style = 'nvchad',
+          },
+        },
+      }
+
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
