c2ae917bd30738928f6b32f07e4dc1b6b336591c
Aidan <aidan.heathfield@gmail.com>
2025-04-21 01:22:30 -0400

    Added tmux navigator and tabout plugins

---
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,22 @@
+-- This will contain useful plugins such as TMUX
+return {
+  {
+    'christoomey/vim-tmux-navigator',
+    lazy = false, -- This basically means that the plugin is loaded immediately when neovim starts
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
+}
